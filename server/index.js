05b81594bc54ce167a7b12c1302b2d82f00ff458
Dheeraj <dheerajdagar94@gmail.com>
2022-02-06 00:05:10 +0530

    node routes setup for apis

--- server/index.js
+++ server/index.js
@@ -2,11 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 // App setup
 const app = express();
+
+app.use('/posts',postRoutes);
+
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
-app.use('cors');
+app.use(cors());
 
 // Mongo setup
+const CONNECTION_URL = "mongodb+srv://ddtest:GWneVcZl273PZ4cl@mern-mongo.95bpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, ()=> console.log('server running on port:'+PORT)))
+.catch((error)=> console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);//make sure we dont get any warnings in console
