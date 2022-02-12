b8be591486f1cb89985340dcce772683b8938d37
Dheeraj <dheerajdagar94@gmail.com>
2022-02-12 23:14:34 +0530

    fixed cors issue

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
--- server/index.js
+++ server/index.js
@@ -7,18 +7,26 @@ import postRoutes from './routes/posts.js';
 // App setup
 const app = express();
 
-app.use('/posts',postRoutes);
+// cors setup
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
-app.use(bodyParser.json({limit:"30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
-app.use(cors());
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 // Mongo setup
 const CONNECTION_URL = "mongodb+srv://ddtest:GWneVcZl273PZ4cl@mern-mongo.95bpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> app.listen(PORT, ()=> console.log('server running on port:'+PORT)))
-.catch((error)=> console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('server running on port:' + PORT)))
+    .catch((error) => console.log(error.message));
 
 //mongoose.set('useFindAndModify', false);//make sure we dont get any warnings in console
