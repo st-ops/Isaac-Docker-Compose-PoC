922860c28f58bc858a72c71cbdb5ddb6f8c49dc1
dheeraj097 <dheerajdagar94@gmail.com>
2022-03-28 17:55:14 +0530

    Create & Edit post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
 import useStyles from './style';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId}) => {
     const [postData, setPostData] = useState({
        creator:'',
        title:'',
@@ -16,23 +17,42 @@ const Form = () => {
        selectedFile:''
     });
 
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(()=>{
+        if(post)
+            setPostData(post);
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        dispatch(createPost(postData));
+        if(currentId){
+            dispatch(updatePost(currentId, postData));
+        }else{
+            dispatch(createPost(postData));
+        }
+        clear()
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({
+            creator:'',
+            title:'',
+            message:'',
+            tags:'',
+            selectedFile:''
+        })
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>Creating a memory</Typography>
+                <Typography variant='h6'>{currentId ? 'Editing' : 'Creating' } a memory</Typography>
                 <TextField name='creator' variant='outlined' label='Creator' fullWidth value={ postData.creator} onChange={ (e) => setPostData({ ...postData, creator: e.target.value})} />
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={ postData.title} onChange={ (e) => setPostData({ ...postData, title: e.target.value})} />
                 <TextField name='message' variant='outlined' label='Message' fullWidth value={ postData.message} onChange={ (e) => setPostData({ ...postData, message: e.target.value})} />
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,24 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post';
 import useStyles from './style';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     console.log(posts);
     const classes = useStyles();
     return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post.id} item xs={12} sm={6}>
+                        <Post post={post} setCurrentId={setCurrentId}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
